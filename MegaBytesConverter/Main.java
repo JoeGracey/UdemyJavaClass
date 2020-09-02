e4e0a29c2fcbd0a36a5d422f12b4434c3ff3d6f9
Joe Gracey <joe.gracey@gmail.com>
2020-09-01 18:23:03 -0600

    Add files via upload

---
+++ MegaBytesConverter/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    /*
+	    Write a method called printMegaBytesAndKiloBytes that has 1 parameter of type int with
+	    the name kiloBytes.
+
+	    The method should not return anything (void) and it needs to calculate the megabytes
+	    and remaining kilobytes from kilobytes parameter.
+
+	    Then it needs to print a message in the format "XX KB = YY MB and ZZ KB".
+
+	    XX represents the original value kiloBytes.
+	    YY represents the calculated megaBytes.
+	    ZZ represents the calculated remaining kiloBytes.
+
+	    For example, when the parameter kiloBytes is 2500 it needs to print
+	    "2500 KB = 2MB and 452 KB"
+
+	    If the parameter kiloBytes is less than 0 then print the text "Invalid Value".
+
+	    Example Input/Output:
+	        printMegaBytesAndKiloBytes(2500); -> "2500 KB = 2 MB and 452 KB"
+	        printMegaBytesAndKiloBytes(-1024); -> "Invalid Value"
+	        printMegaBytesAndKiloBytes(5000); -> "5000 KB = 4 MB and 904 KB"
+
+        TIPS: Use the remainder operator. 1 MB = 1024 KB. 
+	     */
+    }
+}
